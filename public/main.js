6aa8e5c426cb38f603fd9697642a2fa1e08821be
yurievdd <yuriev@comepay.ru>
2020-01-24 20:28:21 +0300

    2020.01.24 Authorization 1.1

--- public/main.js
+++ public/main.js
@@ -1,101 +1,49 @@
-//чек логин:
+// let bearer = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9tdWQtYmFja1wvYXBpXC9sb2dpbiIsImlhdCI6MTU3OTg3MTc1OSwiZXhwIjoxNTc5ODc1MzU5LCJuYmYiOjE1Nzk4NzE3NTksImp0aSI6InNuTUliakNoaUh2ZXlXZWkiLCJzdWIiOjEsInBydiI6Ijg3ZTBhZjFlZjlmZDE1ODEyZmRlYzk3MTUzYTE0ZTBiMDQ3NTQ2YWEifQ.uKMrVR21-TnXYj390bBmA8y4Bd0kT5zXPnx2KWdbLxE';
 
-// jQuery.support.cors = true;
-
-let bearer = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9tdWQtYmFja1wvYXBpXC9sb2dpbiIsImlhdCI6MTU3OTc5OTg2NiwiZXhwIjoxNTc5ODAzNDY2LCJuYmYiOjE1Nzk3OTk4NjYsImp0aSI6InJCRWQyOHNXSFR1enRtSnUiLCJzdWIiOjEsInBydiI6Ijg3ZTBhZjFlZjlmZDE1ODEyZmRlYzk3MTUzYTE0ZTBiMDQ3NTQ2YWEifQ.hSvrquz2hL1m-hhd263Q5CXbCGMd2hqcytN5XruS3qw';
-// $.ajaxSetup({
-//     headers: {
-//         'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-//     }
-// });
+let bearer = localStorage.getItem("token");
 
 
-// fetch("http://mud-back/api/profile", {
-//     method     : 'GET',
-//     crossDomain: true,
-//     mode       : 'cors',
-//     headers    : {
-//         'Authorization': 'Bearer ' + bearer,
-//         'Accept'       : 'application/json',
-//         'Content-Type' : 'application/json'
-//     }
-// });
+function ajaxLogin() {
+    $.ajax({
+        url    : "http://mud-back/api/login",
+        type   : "POST",
+        data   : {
+            'email'   : 'therion@mail.ru',
+            'password': '1'
+        },
+        success: function (response) {
+            console.log('login:');
+            console.log(response);
+            localStorage.setItem("token", response.token);
+        },
+    });
+}
 
-
-// fetch("http://192.168.0.74/web/v1/accounts/me", {
-//     method     : 'GET',
-//     crossDomain: true,
-//     mode       : 'cors',
-//     headers    : {
-//         'Authorization': 'Bearer ' + bearer,
-//         'Accept'       : 'application/json',
-//         'Content-Type' : 'application/json'
-//     }
-// });
-
-
-$.ajax({
-    // url    : "http://mud-back/api/profile",
-    // type   : "GET",
-    // beforeSend: function (xhr) {
-    //     request.withCredentials = true;
-    //     xhr.setRequestHeader('Authorization', 'Bearer ' + bearer);
-    // },
-    // headers: {
-    //     "authorization": "Bearer " + bearer,
-    // },
-    // data   : {
-    // },
-    // jsonp: "callback",
-    // dataType: "jsonp",
-    // cache:  false,
-    // async:  true,
-    // contentType:    'application/json',
-
-
-
-
-    // beforeSend: function (xhr) {
-    //     xhr.setRequestHeader('Authorization', 'Bearer ' + bearer);
-    // },
-    headers: {
-        "authorization": "Bearer " + bearer,
-    },
-    type      : 'GET',
-    // url       : "http://mud-back/api/profile?token=" + bearer,
-    url       : "http://mud-back/api/profile",
-    // dataType: 'json',
-    // dataType  : "jsonp",
-    cache     : false,
-    // async:  true,
-    // xhrFields : {
-    //     withCredentials: true
-    // },
-    // crossDomain: true,
-
-    success: function (response) {
-        // You will get response from your PHP page (what you echo or print)
-        console.log(response);
-    },
-});
-
-
-//OK!
-// $.ajax({
-//     url    : "http://mud-back/api/login",
-//     type   : "POST",
-//     data   : {
-//         'email': 'therion@mail.ru',
-//         'password': '1'
-//     },
-//     success: function (response) {
-//         // You will get response from your PHP page (what you echo or print)
-//         console.log(response);
-//     },
-// });
 
 $(document).ready(function () {
     // console.log( "ready!" );
+
+    if (bearer) {
+        $.ajax({
+            type   : 'GET',
+            url    : "http://mud-back/api/profile?token=" + bearer,
+            cache  : false,
+            success: function (response) {
+                console.log(response);
+                $('.login-logout img').attr("src","public/_images/exit-512.png");
+            },
+            error  : function (jqXHR, textStatus, errorThrown) {
+                console.log('жопа');
+                console.log(jqXHR.status, textStatus, errorThrown);
+                if (jqXHR.status == 401) {
+                    // ajaxLogin();
+                }
+            }
+        });
+
+    } else {
+        ajaxLogin();
+    }
 
     console.log($('#second-row').height());
     console.log($('#main-panel-text div').height());
